3e33b308f2342c2936939baa5785770abf2f6366
Eduardo <costaeduardomaciel@gmail.com>
2022-06-23 20:42:57 -0300

    fix: Styles de componentes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+export const About = () => {
+  return (
+    <section id='about' className='about__container'>
+      <h5>About</h5>
+      <article>
+        <h3>Em breve</h3>
+      </article>
+    </section> 
+  );
+}
---
+++ src/components/feedback/FeedBack.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FeedBack.css';
+
+export const FeedBack = () => {
+  return (
+    <section id='feedback' className='feedback__container'>
+      <h5>Feedback</h5>
+      <article>
+        <h3>Em breve</h3>
+      </article>
+    </section> 
+  );
+}
