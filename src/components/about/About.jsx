5c58b2f8cb3db1e478826188158734025732bce8
Eduardo <costaeduardomaciel@gmail.com>
2022-07-11 21:25:16 -0300

    Feat: Implementa o About e styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './About.css';
+import me from '../../assets/images/me.png';
 
 export const About = () => {
   return (
     <section id='about' className='about__container'>
       <h5>About</h5>
       <article>
-        <h3>Em breve</h3>
+        <div className="img__container">
+          <img src={ me } alt="imagem de Eduardo Costa Maciel" />
+        </div>
+        <div className="text__container">
+          <p>
+            Me chamo Eduardo Costa, tenho 32 anos, meus hobbies favoritos são assistir séries e games, sou do time cachorros, tenho três o Hulk, Boogie e a Mag.
+          </p>
+          <br />
+          <p>
+            Acabei de me formar em desenvolvimento de software na Trybe, busco por oportunidades de iniciar minha carreira como desenvolvedor, e adquirir ainda mais conhecimento em tecnologias diversas.
+          </p>
+        </div>
       </article>
     </section> 
   );
